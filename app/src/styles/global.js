de9f947e9e611105993e76784c4d02804eda09aa
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-09-16 17:12:18 -0300

    Create global.js

---
+++ app/src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle `
+
+    body {
+        padding: 0;
+        margin:0;
+        font-family: 'Poppins' , Sans-Saric;
+        background-color: #f2f2f2;
+
+    }`;
+export default Global;
