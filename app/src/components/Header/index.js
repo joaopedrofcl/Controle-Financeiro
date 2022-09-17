f9ee3e17600e98b6a0078e404ecf419e008e1b68
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-09-17 11:46:39 -0300

    Update index.js

---
+++ app/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+    return(
+        <C.Conteiner>
+            <C.Header> 
+                <C.title>Controle financeiro</C.title>
+            </C.Header>
+        </C.Conteiner>
+    )
+}
+
+export default Header;
