686b98fd3ee38333bbe38f84a87549b8c27ef299
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-09-19 15:17:24 -0300

    Update index.js

--- app/src/components/Resume/index.js
+++ app/src/components/Resume/index.js
@@ -1,25 +1,28 @@
 import React from "react";
 import ResumeItem from "../ResumeItem";
-import * as C from "./styles"
+import * as C from "./styles";
 import {
-    FaRegArrowAltCircleUp,
-    FaRegArrowAltCircleDown,
-    FaDollarSign,
-  } from "react-icons/fa";
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
 
-const Resume = () =>{
-    return(
-    <C.Conteiner>
-        <ResumeItem  title="Entradas"
+const Resume = ({ income, expense, total }) => {
+  return (
+    <C.Container>
+      <ResumeItem
+        title="Entradas"
         Icon={FaRegArrowAltCircleUp}
-        value="1000"/>
-        <ResumeItem title="Saídas"
+        value={income}
+      />
+      <ResumeItem
+        title="Saídas"
         Icon={FaRegArrowAltCircleDown}
-        value="1000"/>
-        <ResumeItem title="Total" Icon={FaDollarSign}
-        value = "1000"/>
-        </C.Conteiner>
-    )
-}
+        value={expense}
+      />
+      <ResumeItem title="Total" Icon={FaDollarSign} value={total} />
+    </C.Container>
+  );
+};
 
 export default Resume;
